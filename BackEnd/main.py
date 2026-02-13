dfe8a3ddc1952c1242cbf07ae4df7038b347aaa6
Dylan Pineda <dyspinedaa@gmail.com>
2026-02-12 21:01:13 -0500

    v0.3

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,2 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI() #Crear el objeto en FastAPI
+@app.get("/sumar") # anotación de lo que va a ejecutar la API (en este caso: sumar)
+def sumar_numeros(a:float, b:float):
+    return a+b
